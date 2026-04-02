dc1ebba924c00cad3a9879340fd3f9b811de729d
Mayank Chugh <mayankchugh-learning@users.noreply.github.com>
2026-04-03 00:42:37 +0800

    Integrate ChatAnthropic model and update main.py; add isort dependency in pyproject.toml and requirements.txt; minor adjustments in weather_server.py for consistency.

--- main.py
+++ main.py
@@ -3,7 +3,19 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-print(f"OPENAI_API_KEY: {os.getenv('OPENAI_API_KEY')}")
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+from langchain_anthropic import ChatAnthropic
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain.agents import create_agent
+
+llm = ChatAnthropic(model="claude-3-5-sonnet-20240620", anthropic_api_key=os.getenv("ANTHROPIC_API_KEY"))
+
+stdio_server = StdioServerParameters(
+   command="python",
+    args ={"C:\\\githunb\mcp-servers\\shellserver\\servers\\math_server.py"}
+)
 
 
 async def main():
